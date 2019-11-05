56fd2258c59d2b184061aa4f8a103719c14a4d82
Amir Mourali <amourali7135@gmail.com>
2019-11-05 15:29:23 +0800

    Dual terminal functionality?

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,26 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if current_user
+      @user = current_user
+      @artist = Artist.find_by(user_id: @user.id) #user_id: refers to artist table one!
+  end
+
+  def dashboard
+  end
+
+  def qrmade
+  end
+
+  def registered
+  end
+
+  def help
+  end
+
+  def contact
+  end
+
+  def about
   end
 end
--- app/models/personal.rb
+++ app/models/personal.rb
@@ -1,3 +1,4 @@
 class Personal < ApplicationRecord
   belongs_to :user
+  uuidable
 end
