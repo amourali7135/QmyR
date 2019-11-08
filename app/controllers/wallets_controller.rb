d128e61424fcd07169126819f7c71fc316508cdf
Amir Mourali <amourali7135@gmail.com>
2019-11-08 19:12:12 +0800

    Wallet working

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -20,8 +20,10 @@ class WalletsController < ApplicationController
     @wallet = current_user.wallet
     # @personals = Personal.where(current_user.transaction.sender_id)
     # @businesses = Business.where(current_user.transaction.sender_id)
-    @personals = Personal.all
-    @businesses = Business.all
+    @personals = (Transaction.where(receiver_id: current_user.wallet.id) + Transaction.where(sender_id: current_user.wallet.id)).map { |tr| [Wallet.find(tr.sender_id).user, Wallet.find(tr.receiver_id).user] }.flatten.reject { |user| user == current_user }.map { |user| user.personal }.compact
+    @businesses = (Transaction.where(receiver_id: current_user.wallet.id) + Transaction.where(sender_id: current_user.wallet.id)).map { |tr| [Wallet.find(tr.sender_id).user, Wallet.find(tr.receiver_id).user] }.flatten.reject { |user| user == current_user }.map { |user| user.business }.compact
+    # raise
+    # @transaction = Transaction.find(params[:id])
   end
 
   def update
