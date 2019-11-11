c29e4f6ae0d50c57d451f16eddcb6c6a2ba9aa83
Amir Mourali <amourali7135@gmail.com>
2019-11-11 20:17:51 +0800

    Transactions fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+  :recoverable, :rememberable, :validatable
+  
   has_one :personal, dependent: :destroy
   has_one :business, dependent: :destroy
   has_one :wallet, dependent: :destroy
-
-
+  has_many :receiver_transactions, through: :wallet, source: :transactions_receiver
+  has_many :sender_transactions, through: :wallet, source: :transactions_sender
+  
   after_create :autocreatewallet
-
+  
   private
-
+  
   def autocreatewallet
     # self.wallet.create!
     Wallet.create(user: self)
   end
-
+  
 end
