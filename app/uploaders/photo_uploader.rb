8a343d270590f209d9fb4edf8033296ce51b2d12
Amir Mourali <amourali7135@gmail.com>
2019-11-24 17:36:18 +0800

    Photo uploader rotated attempt to fix 1

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,13 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-  
+
+  def auto_orient
+    manipulate! do |img|
+      img.auto_orient
+      img
+    end
+  end
+
+  process :auto_orient
+
 end
