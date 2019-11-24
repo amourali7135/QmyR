ab1c51a15273152525bf523c6a8f7ef2b5d95aaf
Amir Mourali <amourali7135@gmail.com>
2019-11-24 17:41:10 +0800

    Revert to test again

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,13 +1,13 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  def auto_orient
-    manipulate! do |img|
-      img.auto_orient
-      img
-    end
-  end
+  # def auto_orient
+  #   manipulate! do |img|
+  #     img.auto_orient
+  #     img
+  #   end
+  # end
 
-  process :auto_orient
+  # process :auto_orient
 
 end
