1e53ac0796422d68e4562301cc816ef3268ff527
Amir Mourali <amourali7135@gmail.com>
2019-11-29 02:47:13 +0800

    front end work and landing page starting to come together yet need to try it on phone

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,17 +1,19 @@
 class Business < ApplicationRecord
   belongs_to :user
-  
+
+  validates :first_name,  presence: true
+
   mount_uploader :photo, PhotoUploader
-  
+
   include PgSearch::Model
   pg_search_scope :global_search,
   against: [ :first_name, :last_name ],
-#   associated_against: {
-#   personal: [ :first_name, :last_name]
-# }, lmfao
-using: {
-tsearch: { prefix: true }
-}
+  #   associated_against: {
+  #   personal: [ :first_name, :last_name]
+  # }, lmfao
+  using: {
+    tsearch: { prefix: true }
+  }
 
   def transaction_time
     created_at.strftime("%m/%d/%y at %l:%M %p")
--- app/models/personal.rb
+++ app/models/personal.rb
@@ -1,17 +1,19 @@
 class Personal < ApplicationRecord
   belongs_to :user
-  
+
+  validates :first_name,  presence: true
+
   mount_uploader :photo, PhotoUploader
-  
+
   include PgSearch::Model
   pg_search_scope :global_search,
   against: [ :first_name, :last_name ],
   # associated_against: {
   # business: [ :first_name, :last_name ]
-# },  lmfao
-using: {
-tsearch: { prefix: true }
-}
+  # },  lmfao
+  using: {
+    tsearch: { prefix: true }
+  }
 
   def transaction_time
     created_at.strftime("%m/%d/%y at %l:%M %p")
